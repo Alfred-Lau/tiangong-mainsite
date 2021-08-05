f74de13eb44d41c6faa8b187b3473f231f21cfe0
Alfred_Lau <jian.liu@icloud.com>
2021-08-05 23:06:09 +0800

    style(flipcard): style change

--- src/components/FlipCard/index.tsx
+++ src/components/FlipCard/index.tsx
@@ -6,24 +6,26 @@ export interface FlipCardProps {
   link: string;
   tags: string[];
   cover: string;
+  long?: string;
 }
 
 const FlipCard: React.FC<FlipCardProps> = (props: FlipCardProps) => {
-  const { title, desc, link, tags = [], cover } = props;
+  const { title, desc, link, tags = [], cover, long } = props;
   return (
     <div className={styles.flip_card_container}>
       <div className={styles.center}>
         <div className={styles.cover}>
           <img src={cover} alt="cover" className={styles.coverImg} />
         </div>
-        <div className={styles.content}>
-          <h4 className={styles.title}>{title}</h4>
-          <p className={styles.desc}>{desc}</p>
+
+        <div className={styles.mask}>
+          <span className={styles.title}>{title}</span>
           <div className={styles.tags}>
             {tags.map((tag) => {
               return <span className={styles.tag}>{tag}</span>;
             })}
           </div>
+          <p className={styles.desc}>{desc}</p>
         </div>
       </div>
     </div>
