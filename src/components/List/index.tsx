183a6e18d380c93cb9f40baf4372cabac34eb366
Alfred_Lau <jian.liu@icloud.com>
2021-08-06 02:36:39 +0800

    style(home): home style

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,11 +1,33 @@
 import styles from './index.less';
 
 export interface ListProps {
-  data: object[];
+  data: ItemProps[];
+  showMore?: boolean;
+}
+
+export interface ItemProps {
+  key: number;
+  title: string;
+  description: string;
+  link: string;
+  created_at: string;
+  author: string;
+}
+
+function Item({ item }) {
+  const { title } = item;
+  return <div>{title}</div>;
 }
 
 function List(props: ListProps) {
-  return <div className={styles.list}>list</div>;
+  const { data, showMore } = props;
+  return (
+    <div className={styles.list}>
+      {data.map((item, index) => {
+        return <Item item={item} key={index} />;
+      })}
+    </div>
+  );
 }
 
 export default List;
