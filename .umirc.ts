dee81bb6de467e2789336313b03461b63e051a6b
Alfred_Lau <jian.liu@icloud.com>
2021-08-14 12:43:06 +0800

    feat(config): add local proxy

--- .umirc.ts
+++ .umirc.ts
@@ -1,5 +1,8 @@
 import { defineConfig } from 'umi';
 import routes from './routes';
+import proxyMap from './proxy';
+type ENV = 'dev' | 'test' | 'pre' | 'prod';
+const NODE_ENV = process.env.NODE_ENV as ENV;
 
 export default defineConfig({
   nodeModulesTransform: {
@@ -10,4 +13,5 @@ export default defineConfig({
   favicon:
     'https://lazy-minus-your-intelligence.oss-cn-qingdao.aliyuncs.com/favicon.ico',
   title: '天工工程体系主站',
+  proxy: proxyMap[NODE_ENV || 'dev'],
 });
