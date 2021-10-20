08c2d646fd8c7ae22c0fdda3d6b45f624455bf8c
Alfred_Lau <jian.liu@icloud.com>
2021-10-20 23:54:19 +0800

    feat: 主站接入cnzz 埋点

--- .umirc.ts
+++ .umirc.ts
@@ -12,4 +12,9 @@ export default defineConfig({
     'https://lazy-minus-your-intelligence.oss-cn-qingdao.aliyuncs.com/favicon.ico',
   title: '天工工程体系主站',
   proxy,
+  scripts: [
+    `
+    document.write(unescape("%3Cspan id='cnzz_stat_icon_1280463003'%3E%3C/span%3E%3Cscript src='https://s9.cnzz.com/z_stat.php%3Fid%3D1280463003' type='text/javascript'%3E%3C/script%3E"));
+    `,
+  ],
 });
