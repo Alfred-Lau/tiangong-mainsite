154d8640824c16d2b7133c3974c4f1e04b6dd122
Alfred_Lau <jian.liu@icloud.com>
2021-10-22 19:31:47 +0800

    feat: 主站添加tdk & 分享设置

--- .umirc.ts
+++ .umirc.ts
@@ -20,4 +20,27 @@ export default defineConfig({
     document.body.removeChild(document.querySelector('a[title="站长统计"]'))
     `,
   ],
+  metas: [
+    {
+      name: 'keywords',
+      content: '工程化, 天工, 烽烟彼岸',
+    },
+    {
+      name: 'description',
+      content: '完善的前端工程化体系，包含脚手架，监控，后台，搭建，组件库等',
+    },
+    {
+      property: 'og:title',
+      content: '烽火彼岸，黄金海滩',
+    },
+    {
+      property: 'og:description',
+      content: '完善的前端工程化体系，包含脚手架，监控，后台，搭建，组件库等',
+    },
+    {
+      property: 'og:image',
+      content:
+        'https://lazy-minus-your-intelligence.oss-cn-qingdao.aliyuncs.com/favicon.ico',
+    },
+  ],
 });
