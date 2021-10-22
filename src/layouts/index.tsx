3b5474f599538e76248af5713141de06b72f4f02
Alfred_Lau <jian.liu@icloud.com>
2021-10-22 19:19:31 +0800

    feat: 主站对接组件库 & 使用了组件库导航

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
+import { Nav } from '@tiangongkit/react-component';
+
 import { useHistory } from 'umi';
 import styles from './index.less';
 
 const IndexLayout: React.FC = (props) => {
   const history = useHistory();
+  const menus = [
+    { id: 0, title: '技术体系', link: '', items: ['产品', '服务'] },
+    { id: 1, title: '团队管理', link: '', items: ['理财', '油腻'] },
+    { id: 2, title: '人生逆旅', link: '', items: ['慈善', '春风十里'] },
+  ];
   return (
     <div className={styles.layout}>
       <header className={styles.header} onClick={() => history.push('/')}>
-        冯唐易老，李广难封
+        <Nav
+          menus={menus}
+          logo="https://lazy-minus-your-intelligence.com/assets/avatar.png"
+        >
+          <span>冯唐易老李广难封</span>
+        </Nav>
       </header>
       {props.children}
     </div>
