b3f72f696d80c0e15dc2c41819872e16c1f9a7fc
Alfred_Lau <jian.liu@icloud.com>
2022-01-13 00:57:25 +0800

    fix: fix request iisues

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,13 +1,15 @@
 import { request, RequestConfig } from 'umi';
 import jscookie from 'js-cookie';
 
+console.log('process.env.NODE_ENV', process.env.NODE_ENV);
+
 const DOMAIN =
   process.env.NODE_ENV === 'prod' ? 'http://139.129.44.2:8082/' : '';
 
 const OPEN_API_DOMAIN =
-  process.env.NODE_ENV === 'prod'
-    ? 'http://api.lazy-minus-your-intelligence.com/'
-    : 'http://localhost:7002';
+  process.env.NODE_ENV !== 'development'
+    ? 'http://api.lazy-minus-your-intelligence.com'
+    : 'http://localhost:7001';
 
 export default (url: string, options?: RequestConfig) => {
   const token = jscookie.get('authorization');
