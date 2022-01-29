268be13b9dc9d0b713390af867e782436ec6b2aa
Alfred_Lau <jian.liu@icloud.com>
2022-01-29 18:48:47 +0800

    feat: profile

--- src/pages/personal/index.tsx
+++ src/pages/personal/index.tsx
@@ -2,7 +2,12 @@ import React, { FC } from 'react';
 import styles from './index.less';
 
 const Personal: FC = () => {
-  return <div className={styles.personal}>personal</div>;
+  return (
+    <div className={styles.personal}>
+      <div>avatar</div>
+      <div>timeline</div>
+    </div>
+  );
 };
 
 export default Personal;
