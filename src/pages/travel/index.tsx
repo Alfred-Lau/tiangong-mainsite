502ec6a2ed817837936f8d5dafb3f79cc60415fd
Alfred_Lau <jian.liu@icloud.com>
2022-02-11 22:05:01 +0800

    feat: add services parts and dest parts

--- src/pages/travel/index.tsx
+++ src/pages/travel/index.tsx
@@ -4,13 +4,63 @@ import Nav from '@/pages/travel/components/Nav';
 import Banner from '@/pages/travel/components/Banner';
 import Layer from '@/pages/travel/components/Layer';
 
+const destinations = [
+  {
+    id: 1,
+    url: '/assets/rome.png',
+    address: 'Rome',
+    cost: '$15K',
+    time: '10 days',
+  },
+  {
+    id: 2,
+    url: '/assets/rome.png',
+    address: 'Rome',
+    cost: '$15K',
+    time: '10 days',
+  },
+  {
+    id: 3,
+    url: '/assets/rome.png',
+    address: 'Rome',
+    cost: '$15K',
+    time: '10 days',
+  },
+];
+
 const Travel: FC = () => {
   return (
     <div className={styles.travel}>
       <Nav></Nav>
       <Banner></Banner>
       <Layer title={'We Offer Best Services'} subtitle={'CATEGORY'}>
-        我是核心类
+        <img
+          src="/assets/sharding.png"
+          alt=""
+          width="153px"
+          className={styles.img}
+        />
+        <ul className={styles.images}>
+          {[1, 2, 3, 4].map((item) => (
+            <li>
+              <img
+                src={`/assets/best-services-0${item}.png`}
+                alt="best-services"
+              />
+            </li>
+          ))}
+        </ul>
+      </Layer>
+      <Layer title={'Top Destinations'} subtitle={'Top Sellings'}>
+        <img src="/assets/ring.png" alt="" className={styles.flower} />
+        <ul className={styles.dest}>
+          {destinations?.map((item) => (
+            <li>
+              <img src={item.url} alt="best-services" />
+              <div className={styles.metadata}></div>
+            </li>
+          ))}
+        </ul>
       </Layer>
     </div>
   );
