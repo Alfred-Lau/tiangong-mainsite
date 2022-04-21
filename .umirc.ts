8d95dcdb0cd230376e79ed6a876dfb1620e939c1
Alfred_Lau <jian.liu@icloud.com>
2022-04-22 00:22:32 +0800

    style: add workbox sw generatSW

--- .umirc.ts
+++ .umirc.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'umi';
+import { GenerateSW } from 'workbox-webpack-plugin';
 import routes from './routes';
 import proxy from './proxy';
 
@@ -47,10 +48,53 @@ export default defineConfig({
         'https://lazy-minus-your-intelligence.oss-cn-qingdao.aliyuncs.com/favicon.ico',
     },
   ],
-  chainWebpack: (memo, { env, webpack, createCSSRule }) => {
-    // // 设置 alias
-    // memo.resolve.alias.set(
-    //
-    // );
+  chainWebpack(memo) {
+    // workbox 配置
+    memo.plugin('workbox').use(GenerateSW, [
+      {
+        cacheId: 'webpack-pwa', // 设置前缀
+        skipWaiting: true, // 强制等待中的 Service Worker 被激活
+        clientsClaim: true, // Service Worker 被激活后使其立即获得页面控制权
+        cleanupOutdatedCaches: true, //删除过时、老版本的缓存
+        swDest: 'service-wroker.js', // 输出 Service worker 文件
+        include: ['**/*.{html,js,css,png.jpg}'], // 匹配的文件
+        exclude: ['service-wroker.js'], // 忽略的文件
+        runtimeCaching: [
+          {
+            urlPattern: /.*\.js.*/i,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'seed-js',
+              expiration: {
+                maxEntries: 20, //最多缓存20个，超过的按照LRU原则删除
+                maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days
+              },
+            },
+          },
+          {
+            urlPattern: /.*css.*/,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'seed-css',
+              expiration: {
+                maxEntries: 30, //最多缓存30个，超过的按照LRU原则删除
+                maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days
+              },
+            },
+          },
+          {
+            urlPattern: /.*(png|svga).*/,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'seed-image',
+              expiration: {
+                maxEntries: 30, //最多缓存30个，超过的按照LRU原则删除
+                maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days
+              },
+            },
+          },
+        ],
+      },
+    ]);
   },
 });
